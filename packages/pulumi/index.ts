964bcd38411b3c461af17a6b6e684964986ffe15
James Hunter <61049336+jimjoes@users.noreply.github.com>
2022-04-12 05:55:31 +0100

    Exported stripe code properly

--- packages/pulumi/index.ts
+++ packages/pulumi/index.ts
@@ -4,8 +4,12 @@ import CloudfrontWebApp from "./src/cloudfrontWebapp";
 import UserGroups from "./src/userGroups";
 import CognitoPasswordless from "./src/cognitoPasswordless";
 import CloudfrontPagebuilderDelivery from "./src/cloudfrontPagebuilderDelivery";
+import StripeCheckout from "./src/stripeCheckout/pulumi";
+import stripeCheckoutHandler from "./src/stripeCheckout/code";
 
 export {
+  stripeCheckoutHandler,
+  StripeCheckout,
   CloudfrontPagebuilderDelivery,
   Blog,
   CloudfrontWebApp,
