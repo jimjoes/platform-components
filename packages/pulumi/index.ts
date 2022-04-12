cbb272ef2a9038b601c81866c89e79741758df8b
James Hunter <61049336+jimjoes@users.noreply.github.com>
2022-04-12 04:19:10 +0100

    fixed not exporting all classes issue

--- packages/pulumi/index.ts
+++ packages/pulumi/index.ts
@@ -1,5 +1,13 @@
-import Cloudfront from "./src/cloudfrontApi";
+import Blog from "./src/blog";
+import CloudfrontApi from "./src/cloudfrontApi";
+import CloudfrontWebApp from "./src/cloudfrontWebapp";
 import UserGroups from "./src/userGroups";
 import CognitoPasswordless from "./src/cognitoPasswordless";
 
-export { Cloudfront, UserGroups, CognitoPasswordless };
+export {
+  Blog,
+  CloudfrontWebApp,
+  CloudfrontApi,
+  UserGroups,
+  CognitoPasswordless,
+};
