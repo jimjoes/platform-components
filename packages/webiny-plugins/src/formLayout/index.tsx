c68577f5f7d8a9aa4f079a4ca6726f559dd808e5
James Hunter <61049336+jimjoes@users.noreply.github.com>
2022-05-03 06:27:29 +0100

    created stub for formlayout

---
+++ packages/webiny-plugins/src/formLayout/index.tsx
@@ -0,0 +1,13 @@
+import { CustomFormLayout } from "./layout";
+
+export default [
+  {
+    name: "form-layout-default",
+    type: "form-layout",
+    layout: {
+      name: "default",
+      title: "Default layout",
+      component: CustomFormLayout,
+    },
+  },
+];
