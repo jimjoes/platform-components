956b2fbef39d554ba62487effbe0dc4b63c20f40
James Hunter <61049336+jimjoes@users.noreply.github.com>
2022-05-17 13:31:08 +0100

    added navigate to root after successful passwordless challenge response

--- packages/amplify-auth-hooks/src/use-confirm-passwordless-challenge.tsx
+++ packages/amplify-auth-hooks/src/use-confirm-passwordless-challenge.tsx
@@ -1,6 +1,7 @@
 import invariant from "tiny-invariant";
 import { Auth } from "@aws-amplify/auth";
 import { ConsoleLogger as Logger } from "@aws-amplify/core";
+import { useNavigate } from "react-router-dom";
 
 import { useAuthContext } from "./use-auth-context";
 import { useCheckContact } from "./use-check-contact";
@@ -8,6 +9,7 @@ import { useCheckContact } from "./use-check-contact";
 const logger = new Logger("useConfirmSignIn");
 
 export const useConfirmPasswordlessChallenge = () => {
+  const navigate = useNavigate();
   invariant(
     Auth && typeof Auth.confirmSignIn === "function",
     "No Auth module found, please ensure @aws-amplify/auth is imported"
@@ -20,6 +22,7 @@ export const useConfirmPasswordlessChallenge = () => {
     try {
       await Auth.sendCustomChallengeAnswer(authData, code);
       checkContact(authData);
+      navigate("/");
     } catch (error) {
       logger.error(error);
       throw error;
