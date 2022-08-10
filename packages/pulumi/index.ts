79e84f0a12ad902363c836853f1f7ec8bdce3be8
James Hunter <61049336+jimjoes@users.noreply.github.com>
2022-08-10 18:14:40 +0100

    added export

--- packages/pulumi/index.ts
+++ packages/pulumi/index.ts
@@ -16,12 +16,17 @@ import subscribeWebinyClickHandler from "./src/subscribe/clickHandler";
 import Secret from "./src/secret";
 import Parameter from "./src/parameter";
 import Route53ARecord from "./src/route53";
-import { Route53HostedZone, Route53NsRecord } from "./src/route53";
+import {
+  Route53HostedZone,
+  Route53NsRecord,
+  Route53MxRecord,
+} from "./src/route53";
 
 export {
   Route53NsRecord,
   Route53ARecord,
   Route53HostedZone,
+  Route53MxRecord,
   Subscribe,
   subscribeHandler,
   subscribeWebinyClickHandler,
