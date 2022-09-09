1ed58e8c25825befa4a69814ec3833c4f89a033a
James Hunter <61049336+jimjoes@users.noreply.github.com>
2022-09-09 07:48:33 +0100

    add vpc support to options

--- packages/pulumi/src/options/index.ts
+++ packages/pulumi/src/options/index.ts
@@ -4,6 +4,11 @@ import policies from "./policies";
 
 interface OptionsParams {
   env: Record<string, any>;
+  vpc?: aws.ec2.Vpc;
+  subnets?: {
+    private: aws.ec2.Subnet[];
+    public: aws.ec2.Subnet[];
+  };
 }
 
 class Options {
@@ -12,7 +17,7 @@ class Options {
   };
   role: aws.iam.Role;
 
-  constructor({ env }: OptionsParams) {
+  constructor({ vpc, subnets, env }: OptionsParams) {
     const roleName = "options-site-api-lambda-role";
     this.role = new aws.iam.Role(roleName, {
       assumeRolePolicy: {
@@ -36,31 +41,50 @@ class Options {
       policyArn: policy.arn.apply((arn) => arn),
     });
 
-    new aws.iam.RolePolicyAttachment(
-      `${roleName}-AWSLambdaBasicExecutionRole`,
-      {
-        role: this.role,
-        policyArn: aws.iam.ManagedPolicy.AWSLambdaBasicExecutionRole,
-      }
-    );
+    if (vpc && subnets) {
+      new aws.iam.RolePolicyAttachment(
+        `${roleName}-AWSLambdaVPCAccessExecutionRole`,
+        {
+          role: this.role,
+          policyArn: aws.iam.ManagedPolicy.AWSLambdaVPCAccessExecutionRole,
+        }
+      );
+    } else {
+      new aws.iam.RolePolicyAttachment(
+        `${roleName}-AWSLambdaBasicExecutionRole`,
+        {
+          role: this.role,
+          policyArn: aws.iam.ManagedPolicy.AWSLambdaBasicExecutionRole,
+        }
+      );
+    }
+
+    const config: any = {
+      runtime: "nodejs14.x",
+      handler: "handler.handler",
+      role: this.role.arn,
+      timeout: 30,
+      memorySize: 128,
+      code: new pulumi.asset.AssetArchive({
+        ".": new pulumi.asset.FileArchive("../code/options/build"),
+      }),
+      environment: {
+        variables: {
+          ...env,
+          AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
+        },
+      },
+    };
+
+    if (vpc && subnets) {
+      config.vpcConfig = {
+        subnetIds: subnets.private.map((subNet) => subNet.id),
+        securityGroupIds: [vpc.defaultSecurityGroupId],
+      };
+    }
 
     this.functions = {
-      options: new aws.lambda.Function("options", {
-        runtime: "nodejs14.x",
-        handler: "handler.handler",
-        role: this.role.arn,
-        timeout: 30,
-        memorySize: 128,
-        code: new pulumi.asset.AssetArchive({
-          ".": new pulumi.asset.FileArchive("../code/options/build"),
-        }),
-        environment: {
-          variables: {
-            ...env,
-            AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
-          },
-        },
-      }),
+      options: new aws.lambda.Function("options", config),
     };
   }
 }
